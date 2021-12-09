87ee8c7da54fac4c1d23a97687e00ddc1692a83f
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2021-12-09 12:10:43 +0100

    Add content to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
-    Home
+    <h1>Star Wars Starship Directory</h1>
+    <img src="../assets/home.jpg" alt="">
+    <div class="info-text">
+      <p>Welcome to the Star Wars Starship Directory. <br/> Once you're logged in, here you will find information about the characteristics
+        of the starships, including its pilots and the films of the saga where they appear.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -15,3 +21,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.info-text {
+  width: 70vw;
+  margin: 20px auto;
+}
+</style>
